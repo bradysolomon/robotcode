41dac56741c2b8b2f2b499e156f09a6d29fdba36
Henri Schmidt <hschmidt16@bsmc02kx0twf5yv.bsm-online.local>
2016-03-18 13:52:34 -0500

    Wrote code to control robot motors with arrow keys.

--- robot controller/robot.py
+++ robot controller/robot.py
@@ -2,6 +2,7 @@ import threading
 import time
 import socket
 import json
+import Adafruit_BBIO.PWM as PWM
 
 SERVER_PORT = 57373
 BUFFER_SIZE = 1024
@@ -17,11 +18,13 @@ class NonAutonomousRobotWorker():
 		self.robot_commands = robot_commands
 
 	def start(self):
+		self._robot_initialization()
 		self.is_thread_running = True
 		self.running_thread = threading.Thread(target=self.robot_loop, args=())
 		self.running_thread.start()
 
 	def stop(self):
+		self._robot_termination()
 		self.is_thread_running = False
 
 	def get_queued_data(self):
@@ -32,13 +35,60 @@ class NonAutonomousRobotWorker():
 	####################################################
 
 	# self.robot_commands is a dictionary of keys and values #
-	# {u'down_key': False, u'up_key': False, u'left_key': False, u'speed': 50.0, u'right_key': False} #
+	# {u'down_key': False, u'up_key': False, u'left_key': False, u'speed': 1.0, u'right_key': False} #
+	# Speed should be between 0.0-1.0 (0 stopped 1 fast)
+
+	# DUTY CYCLE RANGE = 6.5-7.5-8.5
+	def _robot_initialization(self):
+		self._leftdrive = "P9_14"
+		self._rightdrive = "P9_16"
+
+		PWM.start(self._leftdrive)
+		pwm.start(self._rightdrive)
+
+	def _robot_termination(self):
+		PWM.close(self._leftdrive)
+		PWM.close(self._rightdrive)
 
 	def robot_loop(self):
 		while self.is_thread_running:
-			print self.robot_commands
-			time.sleep(2.5)
+			PWM.set_frequency(self._leftdrive, 50)
+			PWM.set_frequency(self._rightdrive, 50)
 
+			# Speed is a value between 0 and 1
+			speed = self.robot_commands["speed"]
+
+			if self.robot_commands["up_key"]:
+				if self.robot_commands["left_key"]:
+					#move robot left and forward
+					PWM.set_duty_cycle(self._rightdrive, 7.5 + speed)
+				elif self.robot_commands["right_key"]:
+					#move robot right and forward
+					PWM.set_duty_cycle(self._leftdrive, 7.5 + speed)
+				else:
+					#move robot purely forwad
+					PWM.set_duty_cycle(self._leftdrive, 7.5 + speed)
+					PWM.set_duty_cycle(self._rightdrive, 7.5 + speed)
+			elif self.robot_commands["down_key"]:
+				if self.robot_commands["left_key"]:
+					#move robot left and backwards
+					PWM.set_duty_cycle(self._rightdrive, 7.5 - speed)
+				elif self.robot_commands["right_key"]:
+					#move robot right and backwards
+					PWM.set_duty_cycle(self._leftdrive, 7.5 - speed)
+				else:
+					#move robot purely backwards
+					PWM.set_duty_cycle(self._leftdrive, 7.5 - speed)
+					PWM.set_duty_cycle(self._rightdrive, 7.5 - speed)
+			elif self.robot_commands["right_key"]:
+				#spin robot right
+				PWM.set_duty_cycle(self._rightdrive, 7.5 - speed)
+				PWM.set_duty_cycle(self._leftdrive, 7.5 + speed)
+			elif self.robot_commands["left_key"]:
+				#spin robot left
+				PWM.set_duty_cycle(self._rightdrive, 7.5 + speed)
+				PWM.set_duty_cycle(self._leftdrive, 7.5 - speed)
+			
 def recvtill(socket, marker):
     # Receive until marker is found, return received message with trailing marker removed 
     buflist = []
